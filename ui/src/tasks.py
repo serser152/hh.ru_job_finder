c330212bf46a6e0bec43ecee8b11e98293ac8298
serser152 <serser152@yandex.ru>
2026-03-31 12:10:50 +0300

    fix

--- ui/src/tasks.py
+++ ui/src/tasks.py
@@ -17,14 +17,14 @@ class ScrapingException(Exception):
         super().__init__(self.msg)
 
 
-#CON = 'postgresql://postgres:postgres@postgres:5432/public'
-#app = Celery('tasks',
-#    backend='redis://redis:6379/0',
-#    broker='amqp://admin:secure_password@rabbitmq//')
+CON = 'postgresql://postgres:postgres@postgres:5432/public'
 app = Celery('tasks',
-    backend='redis://localhost:6380/0',
-    broker='amqp://admin:secure_password@localhost:5673//')
-CON = 'postgresql://postgres:postgres@localhost:5433/public'
+    backend='redis://redis:6379/0',
+    broker='amqp://admin:secure_password@rabbitmq//')
+#app = Celery('tasks',
+#    backend='redis://localhost:6380/0',
+#    broker='amqp://admin:secure_password@localhost:5673//')
+#CON = 'postgresql://postgres:postgres@localhost:5433/public'
 
 
 def get_active_searches():
